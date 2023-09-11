f4e5d535ab93a480b968a60db7f6a98d31c05ac2
Sarah Quinney <quinney.sarah@gmail.com>
2023-09-11 17:47:22 -0500

    updated text and commented out production only things

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog");
-const compression = require("compression");
-const helmet = require("helmet");
+// const compression = require("compression");
+// const helmet = require("helmet");
 
 const app = express();
 
 // Set up rate limiter: maximum of twenty requests per minute
-const RateLimit = require("express-rate-limit");
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
-});
-// Apply rate limiter to all requests
-app.use(limiter);
+// const RateLimit = require("express-rate-limit");
+// const limiter = RateLimit({
+//   windowMs: 1 * 60 * 1000, // 1 minute
+//   max: 20,
+// });
+// // Apply rate limiter to all requests
+// app.use(limiter);
 
 
 
@@ -49,7 +49,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression())
+// app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
