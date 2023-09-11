8bd122f063aaab315d5731421d982ae9aaeb92a9
Sarah Quinney <quinney.sarah@gmail.com>
2023-09-11 17:42:17 -0500

    installed helmet

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog");
 const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 
 
 // Set up mongoose connection
